716c3d79b07397b908266b8415ed2dd339d61e6c
Ricardo <rick_ferry1@hotmail.com>
2025-02-02 21:53:04 -0300

    Ajuste

--- src/main/resources/db/migration/V001__criar_e_registrar_categorias.sql
+++ src/main/resources/db/migration/V001__criar_e_registrar_categorias.sql
@@ -1,11 +1,18 @@
-CREATE TABLE categoria (
-    codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE categoria
+(
+    codigo BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    nome   VARCHAR(255) NOT NULL
+);
 
-INSERT INTO categoria (nome) VALUES ('Alimentação');
-INSERT INTO categoria (nome) VALUES ('Lazer');
-INSERT INTO categoria (nome) VALUES ('Transporte');
-INSERT INTO categoria (nome) VALUES ('Saúde');
-INSERT INTO categoria (nome) VALUES ('Moradia');
-INSERT INTO categoria (nome) VALUES ('Outros');
+INSERT INTO categoria (nome)
+VALUES ('Alimentação');
+INSERT INTO categoria (nome)
+VALUES ('Lazer');
+INSERT INTO categoria (nome)
+VALUES ('Transporte');
+INSERT INTO categoria (nome)
+VALUES ('Saúde');
+INSERT INTO categoria (nome)
+VALUES ('Moradia');
+INSERT INTO categoria (nome)
+VALUES ('Outros');
